1a37c7804234ef6c3f1a5cc99689004f67d11b4f
Rajan Fernandez <rajan.fernandez@gmail.com>
2016-04-10 10:57:29 +1200

    Initial Commit

---
+++ CCSolver/CCSolver.h
@@ -0,0 +1,19 @@
+//
+//  CCSolver.h
+//  CCSolver
+//
+//  Created by Rajan Fernandez on 10/04/16.
+//  Copyright © 2016 Rajan Fernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCSolver.
+FOUNDATION_EXPORT double CCSolverVersionNumber;
+
+//! Project version string for CCSolver.
+FOUNDATION_EXPORT const unsigned char CCSolverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCSolver/PublicHeader.h>
+
+
